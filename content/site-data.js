70917ce44ad3c8fc4d5a774989fed1eb5cba70cf
maijinqiao-hash <maijinqiao@gmail.com>
2026-07-16 01:34:11 +0800

    Add MAIXON V5 website: content/site-data.js

---
+++ content/site-data.js
@@ -0,0 +1,6 @@
+window.MAIXON_SITE_DATA = {
+  version: "5.0.0",
+  downloadUrl: "https://maixon-download.oss-cn-hongkong.aliyuncs.com/downloads/MAIXON_DTF_TOOL_Professional_Setup.exe",
+  seller: "麦经理",
+  phone: "15954870827"
+};
